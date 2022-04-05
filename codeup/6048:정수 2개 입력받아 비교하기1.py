98186dd7f90caae50c2b1c855f9549a6afccd57d
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-04-05 22:19:24 +0900

    Create 6048:정수 2개 입력받아 비교하기1.py

---
+++ codeup/6048:정수 2개 입력받아 비교하기1.py
@@ -0,0 +1,20 @@
+# 두 정수(a, b)를 입력받아 a가 b보다 작으면 True 를, a가 b보다 크거나 같으면 False 를 출력하는 프로그램을 작성해보자.
+
+
+# 내 풀이
+
+a,b = map(int,input().split())
+if a<b:
+    print('True')
+else:
+    print('False')
+ 
+
+
+# 다른 풀이
+ 
+a, b = input().split()
+a = int(a)
+b = int(b)
+print(a<b)
+
