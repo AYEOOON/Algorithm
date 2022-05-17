23a2d5785f6e73398513caafe458260cc0ff3b28
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-05-17 16:09:18 +0900

    Update 6094:이상한 출석 번호 부르기3.py

--- codeup/6094:이상한 출석 번호 부르기3.py
+++ codeup/6094:이상한 출석 번호 부르기3.py
@@ -1,3 +1,10 @@
 # 출석 번호를 n번 무작위로 불렀을 때, 가장 빠른 번호를 출력해 보자.
 # 단, 첫 번째 번호와 마지막 번호가 몇 번인지는 아무도 모른다. 음수(-) 번호, 0번 번호도 있을 수 있다.
 
+
+num = int(input())
+a = list(map(int, input().split()))
+result = a[0]
+for i in a:
+  if result > i: result = i
+print (result)
