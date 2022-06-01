e8fa401c313ca11b2294679af92641d4d6561be9
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-06-01 15:35:05 +0900

    Update 6094:이상한 출석 번호 부르기3.py

--- codeup/6094:이상한 출석 번호 부르기3.py
+++ codeup/6094:이상한 출석 번호 부르기3.py
@@ -2,9 +2,19 @@
 # 단, 첫 번째 번호와 마지막 번호가 몇 번인지는 아무도 모른다. 음수(-) 번호, 0번 번호도 있을 수 있다.
 
 
+# 내풀이
 num = int(input())
 a = list(map(int, input().split()))
 result = a[0]    # result에 0번째 값 잆력
 for i in a:
   if result > i: result = i
 print (result)
+
+
+# 다른 풀이
+num = int(input())
+numlist = map(int, input().split())
+
+a = min(numlist)
+print(a)
+
