c007641d10347de69346ecbf1b01b9530b0f77fd
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-06-01 19:51:01 +0900

    Update 6096:바둑알 십자 뒤집기.py

--- codeup/6096:바둑알 십자 뒤집기.py
+++ codeup/6096:바둑알 십자 뒤집기.py
@@ -22,3 +22,22 @@ for i in range(n):
 
 for i in range(19):
     print(*d[i])
+
+    
+    
+# 다른 풀이
+    
+# 1. 초기 십자 바둑판 입력
+baduk = [[_for in range(19)]for _ in range(19)]
+
+# 2. n과 좌표 값을 입력받고 그에 따라 십자 형태에 맞춰 흑->백, 백->흑으로 변환
+n = int(input())
+for _ in range(n):  # n번입력
+    x, y = map(int(input().split()) # x와 y 좌표 입력
+    for i in range(19): # 변환
+        baduk[i][y-1] = 1 if baduk[i][y-1] == 0 else 0   # 0->1, 1->0으로 변환
+        baduk[x-1][i] = 1 if baduk[x-1][i] == 0 else 0   # 0->1, 1->0으로 변환
+               
+# 3. 변환된 값을 한 줄 단위로 출력
+for b in baduk:
+    print(*baduk[i])           
