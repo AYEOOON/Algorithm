2e16e52665765cfc89cb1e49c125814bd6ccc387
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-06-07 13:05:01 +0900

    Update 14681:사분면 고르기.py

--- BOJ/조건문/14681:사분면 고르기.py
+++ BOJ/조건문/14681:사분면 고르기.py
@@ -1,7 +1,8 @@
 # 점의 좌표를 입력받아 그 점이 어느 사분면에 속하는지 알아내는 프로그램을 작성하시오. 단, x좌표와 y좌표는 모두 양수나 음수라고 가정한다.
 
 
-x,y = map(int,input().split())
+x= int(input())
+y= int(input())
 
 if x > 0 and y > 0 :	# x,y: 양수
     print('1')
