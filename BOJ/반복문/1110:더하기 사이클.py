ecf4bd70c7db23f30ccf4914ebb3f6a77928f6bd
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-06-29 21:43:32 +0900

    Update 1110:더하기 사이클.py

--- BOJ/반복문/1110:더하기 사이클.py
+++ BOJ/반복문/1110:더하기 사이클.py
@@ -1,1 +1,18 @@
 # N이 주어졌을 때, N의 사이클의 길이를 구하는 프로그램을 작성하시오.
+
+
+n = int(input())         # 68
+num = n
+cnt = 0                  # 사이클 수
+
+while True:              # while 1이랑 동일
+    a = num / / 10       # 6
+    b = num % 10         # 8
+    c = (a + b) % 10     # 6 + 8 = 1"4"
+    num = (b * 10) + c   # 80 + 4 = 84
+
+    cnt = cnt + 1        # 사이클 수 + 1
+    if(num == n):        # num에서 입력된 n과 똑같은 숫자(68)가 나오면 멈춤
+        break
+
+print(cnt)
