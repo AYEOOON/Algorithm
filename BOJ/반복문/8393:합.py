7630b447de16b35034939bd52f9fab603c2d9878
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-07-01 23:14:57 +0900

    Update 8393:합.py

--- BOJ/반복문/8393:합.py
+++ BOJ/반복문/8393:합.py
@@ -2,7 +2,7 @@
 
 
 a = int(input())
-sum = 0
-for i in range(a+1):
-    sum = sum + i
+sum = 0       #임의로 정한 변수의 값을 0으로 지정해줌
+for i in range(a+1):    #1부터 n까지의 합이라고 했으니, range의 범위는 n+1가 된다. 1부터 n까지 i에 대입
+    sum = sum + i    #a에 i의 값을 누적해서 더한다.
 print(sum)
