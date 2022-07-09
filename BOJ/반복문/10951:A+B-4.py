ba66e234ebd17ffb2a88465d9c6b8341ec74fdf4
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-07-09 21:34:52 +0900

    Update 10951:A+B-4.py

--- BOJ/반복문/10951:A+B-4.py
+++ BOJ/반복문/10951:A+B-4.py
@@ -1,13 +1,12 @@
 # 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 # 이 문제는 EOF(예외처리)를 사용하여 푸는 문제이다.
 # 예외처리에 대해 공부하기
-# 마지막 풀이에 대해 공부하기
 
-# 내 풀이
+
+# 내 풀이(예외 처리를 사용하는 방법)
 import sys
 
-input = sys.stdin.readline
-
+input = sys.stdin.readline   # sys.stdin.readline() 을 사용했을 경우
 while True:
 
   try:
@@ -21,15 +20,17 @@ while True:
 # 다름사람 풀이(1)
 try:
   while 1:
-    a, b = map(int,input().split())
+    a, b = map(int,input().split())  # input() 를 사용했을 경우.(input() 같은 경우, 더이상 값을 읽어오지 못하게 되면 EOFError 가 발생하는데, 그러한 에러를 무시하고 프로그램 정상 종료.)
     print(a+b)
 except:
   exit()
   
   
-# 다른사람 풀이(2)
+# 다른사람 풀이(2)(예외 처리 없이  sys.stdin.readlines() 를 사용하는 방법)
 import sys
 
-for i in sys.stdin:
-    a, b = map(int,i.split())
-    print(a+b)
+lines = sys.stdin.readlines()
+
+for line in lines:
+	a,b = map(int,line.split())
+    print (a+b)
