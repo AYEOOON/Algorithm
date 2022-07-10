b8a260043683920d756c5ba00419f6997157a012
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-07-10 22:07:34 +0900

    Update 8393:합.py

--- BOJ/반복문/8393:합.py
+++ BOJ/반복문/8393:합.py
@@ -1,8 +1,14 @@
 # n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
 
 
+# 내 풀이
 a = int(input())
 sum = 0       #임의로 정한 변수의 값을 0으로 지정해줌
 for i in range(a+1):    #1부터 n까지의 합이라고 했으니, range의 범위는 n+1가 된다. 1부터 n까지 i에 대입
     sum = sum + i    #a에 i의 값을 누적해서 더한다.
 print(sum)
+
+
+# 다른사람 풀이
+n = int(input())
+print((n**2+n)//2)
