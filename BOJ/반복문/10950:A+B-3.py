1fd8e4104df72c5b2e68d071f0a8f5d837bb34dc
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-07-11 21:56:56 +0900

    Update 10950:A+B-3.py

--- BOJ/반복문/10950:A+B-3.py
+++ BOJ/반복문/10950:A+B-3.py
@@ -3,12 +3,21 @@
 # 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
 
-
+# 내 풀이
 T = int(input())       # T를 입력받음   
 for i in range(T):
     A, B = map(int,input().split())
     print(A+B)
 
+    
+# 다른사람 풀이    
+import sys
+
+input()
+s = list(sum(map(int, n.split())) for n in sys.stdin)
+
+for n in s:
+  print(n)
     
     
 # range안에서 단일로 숫자를 넣게 되면 해당 횟수만큼 for문이 돌게 된다. 
