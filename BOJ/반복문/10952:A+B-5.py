a873b12f9d348851b3fc191ad6154afdf980cb00
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-07-12 21:10:30 +0900

    Update 10952:A+B-5.py

--- BOJ/반복문/10952:A+B-5.py
+++ BOJ/반복문/10952:A+B-5.py
@@ -1,9 +1,8 @@
 # 두 정수 A와B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
 
 
-While True:  #  While문을 True로 두어 계속 반복하게 함
-    A, B = map(int,input().spilt())
-    if ((A == 0) and (B == 0)):
+ while True:#  While문을 True로 두어 계속 반복하게 함
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
         break
-    else:
-        print(A+B)
+    print(a+b)
