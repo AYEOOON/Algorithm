f007046cd9faf0ef7c74b76c63c8b023c09c99d2
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-07-19 21:32:23 +0900

    Update 2480:주사위 세개.py

--- BOJ/조건문/2480:주사위 세개.py
+++ BOJ/조건문/2480:주사위 세개.py
@@ -1,7 +1,7 @@
 # 3개 주사위의 나온 눈이 주어질 때, 상금을 계산하는 프로그램을 작성 하시오.
 
 
-
+# 내 풀이
 a, b, c = map(int,input().split())  # 주사위 3개의 수 입력
 
 if a == b == c :     # 3개 모두 같을 때
@@ -15,3 +15,15 @@ elif b == c:
 else:                  # 3개 다 다를 때
     M = max(a,b,c)     # 가장 큰 수
     print(M*100)
+
+    
+# 다른사람 풀이
+
+lst = sorted(list(map(int, input().split())))
+
+if len(set(lst)) is 1:
+    print(10000 + lst[0]*1000)
+elif len(set(lst)) is 2:
+    print(1000 + lst[1]*100)  # pick middle
+else:
+    print(lst[2]*100)
