d5f300700d8a58f6739b361a744e1489077acb08
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-09-15 22:41:08 +0900

    Update 2753:윤년.py

--- BOJ/조건문/2753:윤년.py
+++ BOJ/조건문/2753:윤년.py
@@ -3,21 +3,24 @@
 
 
 # 내 풀이
+
 year = int(input())
 
-if (year%4 == 0 and (year%100 != 0 or year%400 == 0)) :
+if (year%4 == 0 and (year%100 != 0 or year%400 == 0)) :     # year이 4의 배수이고 100의 배수가 아니거나 또는 400의 배수일 때 if문
     print('1')
 else:
     print('0')
     
     
 # 삼항 연산자로 푼 다른 풀이
+
 year = int(input())
 
 print('1') if ((year%4 == 0)and(year%100 != 0)) or (year%400 == 0) else print('0')
 
 
 # 다른사람 풀이
+
 a=int(input())
 
 if a%4==0 and a%100!=0 or a%400==0:
