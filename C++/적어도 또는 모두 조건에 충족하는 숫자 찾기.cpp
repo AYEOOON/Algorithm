6ebd8ebf47814a300c850e6782c16c715807d8fb
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-09-28 01:02:11 +0900

    Update 적어도 또는 모두 조건에 충족하는 숫자 찾기.cpp

--- C++/적어도 또는 모두 조건에 충족하는 숫자 찾기.cpp
+++ C++/적어도 또는 모두 조건에 충족하는 숫자 찾기.cpp
@@ -3,22 +3,31 @@ using namespace std;
 
 int main()
 { 
-  // 변수 선언
-  int size;
-  bool success = false;
-  int item;
-  // 입력
-  do
-  {
-    cout << "크기를 입력해주세요: ";
-    cin >> size;    
-  } while(size < 0);
-  // counter가 size보다 작은 경우 반복
-  for(int i = 1; (i < size) && (!success); i++)
-  {
-    cout << "숫자를 입력하세요: ";
-    cin >> item;
-    
-  }
-  return 0;
+    // 변수 선언
+    bool success = false;
+    int size;
+    int item;
+    // 입력
+    do
+    {
+        cout << "입력할 횟수를 입력해주세요: ";
+        cin >> size;    
+    } while(size < 0);
+    // counter가 size보다 작은 경우 반복
+    for(int i = 0; (i < size) && (!success); i++)
+    {
+        cout << "숫자를 입력하세요: ";
+        cin >> item;
+        success = (item % 5==0);
+    }
+    // 성공하는 조건
+    if(success)
+    {
+        cout << item << "은/는 5로 나눌 수 있습니다." << endl;
+    }
+    else
+    {
+        cout << "숫자들 중에 5로 나눌 수 있는 수가 없습니다." << endl;
+    }
+    return 0;
 }
