ec1981d737d7f708b9e6891d5ab746cde40d9e6f
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-10-01 22:32:22 +0900

    Update 10989:수 정렬하기3.py

--- BOJ/정렬/10989:수 정렬하기3.py
+++ BOJ/정렬/10989:수 정렬하기3.py
@@ -1,6 +1,8 @@
 # 메모리가 8MB로 제한된 상태로 N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하는 문제
 # 수 정렬하기2에서 썼던 sort()를 사용하면 메모리가 초과된다.
 # 수를 정렬하기 위해서는 숫자가 들어오면 그 숫자의 위치를 1씩 카운트를 하여 출력하면 된다. 
+
+
 # 내 풀이
 
 import sys    # input()을 쓰면 속도가 느려져 sys라이브러리를 호출
@@ -18,3 +20,21 @@ for i in range(10001) :      # 입력받는 숫자는 0부터 10000사이의 숫
     if num[i] != 0 :
         for j in range(num[i]) :    # 해당 리스트를 처음부터 반복문을 돌면서 0이 아닐 경우 해당 숫자만큼 temp를 출력
             print(i)
+
+            
+            
+# 다른사람 풀이
+
+import sys
+input = sys.stdin.buffer.readline
+
+counts = [0 for i in range(10010)]
+
+for n in range(int(input())):
+    counts[int(input())]+=1
+    
+for i in range(10010):
+    for j in range(counts[i]):
+        sys.stdout.write(str(i)+"\n")
+        
+        
