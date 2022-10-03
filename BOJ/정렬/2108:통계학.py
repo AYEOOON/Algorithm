e19377b904edc66c63326ffb8a11da3008b9fb8d
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-10-03 22:26:35 +0900

    Update 2108:통계학.py

--- BOJ/정렬/2108:통계학.py
+++ BOJ/정렬/2108:통계학.py
@@ -1,3 +1,8 @@
+# N이 홀수라고 가정할 때, 산술평균, 중앙값, 최빈값, 범위(N개의 수들 중 최댓값과 최솟값의 차이)를 구하여라
+# 산술평균은 소수점 이하 첫째 자리에서 반올림한다. 최빈값이 여러개면 최빈값 중 두번째로 작은 값을 출력한다. 
+# 최빈값을 구하는 것이 가장 어려웠으며, 시간초과가 뜨지않도록 주의해야한다. 
+
+#  내 풀이(시간초과)
 n = int(input())
 
 nums = []
@@ -40,3 +45,29 @@ else :
 
 # 범위
 print(nums[-1]-nums[0])
+
+
+
+# 다른사람 풀이(counter을 사용하여 푼 풀이)
+
+import sys
+from collections import Counter
+
+n = int(sys.stdin.readline())
+
+nums = []
+for i in range(n):   # 입력받은 n의 수만큼 숫자 입력
+    nums.append(int(sys.stdin.readline()))   # nums 리스트에 넣기
+
+nums.sort()   # 리스트 정렬
+nums_s = Counter(nums).most_common()
+print(round(sum(nums) / n))   # 산술평균
+print(nums[n // 2])     # 중앙값
+if len(nums_s) > 1:     # 최빈값
+    if nums_s[0][1] == nums_s[1][1]:
+        print(nums_s[1][0])
+    else:
+        print(nums_s[0][0])
+else:
+    print(nums_s[0][0])
+print(nums[-1] - nums[0])  # 범위
