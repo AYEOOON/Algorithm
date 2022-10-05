d82b3b325b9153032ffc2020968ae37af675e51e
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-10-05 22:52:37 +0900

    Create 1427:소트인사이드.py

---
+++ BOJ/정렬/1427:소트인사이드.py
@@ -0,0 +1,17 @@
+# 정렬하려고 하는 수 n이 주어지면, 그 수의 각 자리수를 내림차순으로 정렬하는 문제
+
+
+# 내 풀이
+
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+nums = list(map(int,str(n)))
+
+nums.sort(reverse = True)   # sort함수를 이용하여 list에 들어있는 숫자들을 내림차순시킨다. 
+
+for i in nums:    # 정렬된 리스트를 한개씩 출력시킨다. 옵션으로 end=''를 주어 한줄에 출력이 되도록한다. 
+  print(i,end='')
+
+  
