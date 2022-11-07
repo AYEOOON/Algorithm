b18c924abb05c21b4d13552ef6663d48631af4f5
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-11-07 21:08:21 +0900

    Update 5597:과제 안 내신 분..?.py

--- BOJ/1차원 배열/5597:과제 안 내신 분..?.py
+++ BOJ/1차원 배열/5597:과제 안 내신 분..?.py
@@ -17,3 +17,14 @@ for _ in range(28):
 
 print(min(students))
 print(max(students))
+
+
+
+# 다른 사람 풀이
+
+x=list(map(int,range(31)))
+del x[0]
+for i in range(28):
+    x.remove(int(input()))
+for j in x:
+    print(j)
