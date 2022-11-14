ac6b014a573e129510066c0fda494cbd8dd5c2b2
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-11-14 20:51:55 +0900

    Update 05 (strcat).cpp

--- C++/05 (strcat).cpp
+++ C++/05 (strcat).cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
   string str1("The time has come. ");
-  const string str2("Are your ready?");
+  const string str2("Are you ready?");
 
   str1.append(str2);
   
