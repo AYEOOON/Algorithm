b8cbf68b8112ed6f910199b1db101ca89f8057d1
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-11-17 22:13:18 +0900

    Create 2447:별 찍기.py

---
+++ BOJ/재귀/2447:별 찍기.py
@@ -0,0 +1,30 @@
+# 재귀적인 패턴으로 별을 찍어 보자. N이 3의 거듭제곱(3, 9, 27, ...)이라고 할 때, 크기 N의 패턴은 N×N 정사각형 모양이다.
+# N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의 (N/3)×(N/3) 정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다. 
+# 첫째 줄에 N이 주어진다. N은 3의 거듭제곱이다. 즉 어떤 정수 k에 대해 N=3k이며, 이때 1 ≤ k < 8이다.
+# 오랫동안 고민했지만 풀지 못하여 제일 이해하기 쉬운 풀이를 적었다. 
+
+
+
+# 풀이
+
+import sys
+input = sys.stdin.readline
+
+def draw_stars(n):
+  if n==1:
+    return ['*']
+
+  Stars=draw_stars(n//3)
+  L=[]
+
+  for star in Stars:
+    L.append(star*3)
+  for star in Stars:
+    L.append(star+' '*(n//3)+star)
+  for star in Stars:
+    L.append(star*3)
+
+  return L
+
+N=int(input())
+print('\n'.join(draw_stars(N)))
