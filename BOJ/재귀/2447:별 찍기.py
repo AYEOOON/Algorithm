0ace077d4e941034282a37a1abc69c813a6838c1
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-11-21 23:30:29 +0900

    Update 2447:별 찍기.py

--- BOJ/재귀/2447:별 찍기.py
+++ BOJ/재귀/2447:별 찍기.py
@@ -28,3 +28,17 @@ def draw_stars(n):
 
 N=int(input())
 print('\n'.join(draw_stars(N)))
+
+
+# 다른풀이
+
+a=int(input())
+def s(n):
+ if n==3:return['***','* *','***']
+ x=s(n//3)
+ y=list(zip(x,x,x))
+ for i in range(len(y)):y[i]=''.join(y[i])
+ z=list(zip(x,[' '*(n//3)]*(n//3),x))
+ for i in range(len(z)):z[i]=''.join(z[i])
+ return y+z+y
+print('\n'.join(s(a)))
