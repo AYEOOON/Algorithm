1ee3cda4f9c5b9d47dd1c2756d97742bb5887faa
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-12-07 23:41:27 +0900

    Update 6097:설탕과자 뽑기.py

--- codeup/6097:설탕과자 뽑기.py
+++ codeup/6097:설탕과자 뽑기.py
@@ -6,19 +6,19 @@
 
 
 h,w = map(int,input().split())
-n = int(input())
+n = int(input())                                      # 막대의 갯수 n을 입력받는다.
 grid = [list(0 for i in range(w)) for j in range(h)]
 
 for i in range(n):
-  l,d,x,y = map(int,input().split())
+  l,d,x,y = map(int,input().split())                  # n만큼 l(막대의 길이), d(방향), x, y 좌표를 입력받는다.
   x -= 1
   y -= 1
-  if d == 0:
-    for j in range(l):
-      grid[x][y+j] = 1
+  if d == 0:                                         # d가 0(가로)일 경우 if문
+    for j in range(l):                               # 막대 l의 길이 만큼 for문 반복
+      grid[x][y+j] = 1                               # 판에서 y좌표에 막대 길이만큼 1로 변경하여 막대를 표시
   else:
     for j in range(l):
-      grid[x + j][y] = 1
+      grid[x + j][y] = 1                               # 판에서 x좌표에 막대 길이만큼 1로 변경하여 막대를 표시
 
 for crd in grid:
   print(*crd)
