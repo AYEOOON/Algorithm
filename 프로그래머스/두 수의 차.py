12eac096e079d663b064091733c08686905d28ec
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-12-28 21:52:44 +0900

    Create 두 수의 차.py

---
+++ 프로그래머스/두 수의 차.py
@@ -0,0 +1,12 @@
+# 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성하는 문제.
+
+
+# 내 풀이
+
+def solution(num1, num2):
+    answer = num1-num2
+    return answer
+
+# 다른사람 풀이
+
+solution = lambda num1, num2 : num1 - num2
