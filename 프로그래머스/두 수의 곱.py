a0b46c046592dc6f6884d053ae4131d01e0dbaaf
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2022-12-28 21:50:39 +0900

    Create 두 수의 곱.py

---
+++ 프로그래머스/두 수의 곱.py
@@ -0,0 +1,13 @@
+# 정수 num1, num2가 매개변수 주어지고, num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성하는 문제.
+
+
+# 내 풀이
+
+def solution(num1, num2):
+    answer = num1*num2
+    return answer
+  
+  
+# 다른 사람 풀이
+
+solution = lambda num1, num2 : num1 * num2
