b49c696caa4906db94f1705eb1f812c0730d77b2
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2023-05-24 21:25:37 +0900

    Update 2164:카드2.py

--- BOJ/큐/2164:카드2.py
+++ BOJ/큐/2164:카드2.py
@@ -3,16 +3,33 @@
 # N장의 카드가 있다. 각각의 카드는 차례로 1부터 N까지의 번호가 붙어 있으며, 1번 카드가 제일 위에, N번 카드가 제일 아래인 상태로 순서대로 카드가 놓여 있다.
 # 1장이 남을 때까지 반복한다. 
 # N이 주어졌을 때, 제일 마지막에 남게 되는 카드를 구하는 프로그램을 작성하는 문제
-# 리스트는 젤 앞의 값을 뺄 수 없어서 deque을 사용하였다.
+# 리스트를 쓰면 시간초과가 발생하기 때문에 deque를 사용하였다. 
 
+# list를 사용한 풀이
+
+N = int(input())
+L = list(range(1,N+1))
+
+
+while (len(L)>1):
+    L.pop(0)
+    temp = L.pop(0)
+    L.append(temp)
+
+
+print(L.pop(0))
+
+
+
+# deque을 이용한 풀이
 
 from collections import deque
 import sys
-N = int(sys.stdin.readline())
+N = int(sys.stdin.readline())      
 
 card = deque()
 
-for i in range(N):
+for i in range(N):         # card = deque([i for i in range(1, N+1)]) 이렇게 표현할 수도 있다!
   card.append(i+1)
   
 while(len(card) != 1):
