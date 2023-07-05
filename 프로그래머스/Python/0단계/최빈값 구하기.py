fcd7da7765ba4fd05a933219254ec5084980ae18
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2023-07-05 16:58:03 +0900

    Update 최빈값 구하기.py

--- 프로그래머스/Python/0단계/최빈값 구하기.py
+++ 프로그래머스/Python/0단계/최빈값 구하기.py
@@ -1,38 +1,22 @@
 # array가 매개변수로 주어질 때, 최빈값을 return 하도록 solution 함수를 완성하는 문제. 
 # 최빈값이 여러 개면 -1을 return 합니다.
-# 문제를 풀지 못함
+
+# 내 풀이
+def solution(array):
+    arr =[]
+    li = list(set(array))    # li = [1,2,3,4]
+    for i in li:
+        arr.append(array.count(i))  # arr = [1,1,3,1]
+        
+    answer = -1
+    if arr.count(max(arr)) == 1:
+        return li[arr.index(max(arr))]
+    return answer
 
 
-# 풀이1
-
-def solution(array: list) -> int:
-    dict = {}
-
-    # for 반복문으로 입력 list 순회
-    for num in array:
-        # 딕셔너리에 현재 값이 할당되있지 않다면 1 할당
-        if num not in dict:
-            dict[num] = 1
-        # 그렇지 않을 시, 증감
-        else:
-            dict[num] += 1
-            
-    # 딕셔너리의 벨류값 기준으로 desc 정렬
-    result = sorted(dict.items(), key=lambda x: x[-1], reverse=True)
-    
-    if len(result) <= 1:
-        return result[0][0]
-    # 최빈값이 여러개면, -1 반환
-    return result[0][0] if result[0][-1] != result[1][-1] else -1
-
-if __name__ == '__main__':
-    print(solution([1, 2, 3, 3, 3, 4]))  # 3
-    print(solution([1, 1, 2, 2]))  # -1
-    print(solution([1]))  # 1
-
-
-# 풀이2
-
+# 다른사람 풀이2
+# 하나씩 지우면서 마지막에 남는것을 리턴
+# 최빈값이 여러개라면 array에서 숫자가 남으므로 -1을 리턴
 def solution(array):
     while len(array) != 0:
         for i, a in enumerate(set(array)):
