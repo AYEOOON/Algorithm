049ec783a1f2ebc3ede7382932a532edef2a9e38
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2023-07-17 16:26:16 +0900

    Update 대문자와 소문자.js

--- 프로그래머스/JavaScript/0단계/대문자와 소문자.js
+++ 프로그래머스/JavaScript/0단계/대문자와 소문자.js
@@ -1,3 +1,7 @@
+// 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+// 자바스크립트에는 isUpper이 없기때문에 대문자와 비교를 하여 구분해야한다. 
+// 자바스크립트에는 toUpperCase(), toLowerCase() 로 대소문자 전환한다.
+
 // 내 풀이
 function solution(my_string) {
     let answer = ''
