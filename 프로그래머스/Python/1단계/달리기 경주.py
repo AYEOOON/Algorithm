ac5b2e13577f94768ffba65804a81ed8543705cc
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2023-07-24 20:58:37 +0900

    Create 달리기 경주.py

---
+++ 프로그래머스/Python/1단계/달리기 경주.py
@@ -0,0 +1,32 @@
+# 해설진들은 선수들이 자기 바로 앞의 선수를 추월할 때 추월한 선수의 이름을 부릅니다.
+# 선수들의 이름이 1등부터 현재 등수 순서대로 담긴 문자열 배열 players와 해설진이 부른 이름을 담은 문자열 배열 callings가 매개변수로 주어질 때, 경주가 끝났을 때 선수들의 이름을 1등부터 등수 순서대로 배열에 담아 return 하는 solution 함수를 완성해주세요.
+# 기존의 swap하는 방식은 런타임에러를 발생 시킨다. 
+# 따라서 딕셔너리를 사용하여 해결하였다.
+
+
+# 내 풀이
+def solution(players, callings):
+    dic = {player:i for i,player in enumerate(players)}  # 선수:등수
+    
+    for i in callings:
+        index = dic[i]  # 호명된 선수의 현재 등수
+        
+        players[index], players[index-1] = players[index-1],players[index] # 위치 변경
+        
+        dic[players[index]] = index  # 앞사람에게 호명된 선수의 등수를 할당
+        dic[players[index-1]] = index-1  # 호명된 선수의 등수-1
+        
+    return sorted(dic,key = lambda x: dic[x])
+
+
+# 다른사람 풀이
+def solution(players, callings):
+    pla_dic = {key: i for i, key in enumerate(players)}
+
+    for p in callings:
+        c = pla_dic[p]
+        pla_dic[p] -= 1
+        pla_dic[players[c-1]] += 1
+        players[c-1], players[c] = players[c], players[c-1]
+
+    return players
