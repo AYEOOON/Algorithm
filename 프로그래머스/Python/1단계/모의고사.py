8efd8e74978fcc7b680d6ffac5ea040c755cdaf6
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2023-07-25 17:30:47 +0900

    Create 모의고사.py

---
+++ 프로그래머스/Python/1단계/모의고사.py
@@ -0,0 +1,74 @@
+# 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+# 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+# 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+# 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+# 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+
+
+# 내 풀이
+def solution(answers):
+    answer = []
+    person = [0]*3
+    first = [1,2,3,4,5]
+    second = [2,1,2,3,2,4,2,5]
+    third = [3,3,1,1,2,2,4,4,5,5]
+    
+    for i in range(len(answers)):
+        if answers[i] == first[i%5]:
+            person[0] += 1
+        if answers[i] == second[i%8]:
+            person[1] += 1
+        if answers[i] == third[i%10]:
+            person[2] += 1
+        
+    win = max(person)
+    for i in range(len(person)):
+        if win == person[i]:
+            answer.append(i+1)
+    return answer
+    
+        
+
+# 다른사람 풀이1
+def solution(answers):
+    pattern1 = [1,2,3,4,5]
+    pattern2 = [2,1,2,3,2,4,2,5]
+    pattern3 = [3,3,1,1,2,2,4,4,5,5]
+    score = [0, 0, 0]
+    result = []
+
+    for idx, answer in enumerate(answers):
+        if answer == pattern1[idx%len(pattern1)]:
+            score[0] += 1
+        if answer == pattern2[idx%len(pattern2)]:
+            score[1] += 1
+        if answer == pattern3[idx%len(pattern3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            result.append(idx+1)
+
+    return result
+
+
+# 다른사람 풀이2
+from itertools import cycle  # 리스트내 자료를 무한 반복해야하는 경우 itertools의 cycle를 사용
+
+def solution(answers):
+    giveups = [
+        cycle([1,2,3,4,5]),
+        cycle([2,1,2,3,2,4,2,5]),
+        cycle([3,3,1,1,2,2,4,4,5,5]),
+    ]
+    scores = [0, 0, 0]
+    for num in answers:
+        for i in range(3):
+            if next(giveups[i]) == num:
+                scores[i] += 1
+    highest = max(scores)
+
+    return [i + 1 for i, v in enumerate(scores) if v == highest]
