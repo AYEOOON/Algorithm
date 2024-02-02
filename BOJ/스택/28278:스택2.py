072bfe9f46ef08e062eb4afaa69699e9ebcfc8c8
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2024-02-02 22:39:51 +0900

    Create 28278:스택2.py

---
+++ BOJ/스택/28278:스택2.py
@@ -0,0 +1,45 @@
+"""
+정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+
+명령은 총 다섯 가지이다.
+
+1 X: 정수 X를 스택에 넣는다. (1 ≤ X ≤ 100,000)
+2: 스택에 정수가 있다면 맨 위의 정수를 빼고 출력한다. 없다면 -1을 대신 출력한다.
+3: 스택에 들어있는 정수의 개수를 출력한다.
+4: 스택이 비어있으면 1, 아니면 0을 출력한다.
+5: 스택에 정수가 있다면 맨 위의 정수를 출력한다. 없다면 -1을 대신 출력한다.
+"""
+
+# 내 풀이
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = []
+
+for i in range(N):
+  command = input()  # 명령을 입력받을 때는 문자열로 입력받은 뒤 쪼개면 된다. 
+  if command[0] == '1':
+    stack.append(int(command[2:len(command)-1]))  # 여기가 실패의 원인이었다. 입력받는 x가 한자리라는 말이 없다!
+
+  if command[0] == '2':
+    if stack:
+      print(stack.pop())
+    else:
+      print(-1)
+
+  if command[0] == '3':
+    print(len(stack))
+
+  if command[0] == '4':
+    if stack:
+      print(0)
+    else:
+      print(1)
+
+  if command[0] == '5':
+    if stack:
+      print(stack[-1])
+    else:
+      print(-1)
