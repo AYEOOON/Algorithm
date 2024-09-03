adb68ba1229f408ac5b431ba881fdeba8ec11b68
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2024-09-03 21:28:29 +0900

    Create 전국 대회 선발 고사.py

---
+++ 프로그래머스/Python/0단계/전국 대회 선발 고사.py
@@ -0,0 +1,33 @@
+"""
+0번부터 n - 1번까지 n명의 학생 중 3명을 선발하는 전국 대회 선발 고사를 보았습니다. 등수가 높은 3명을 선발해야 하지만, 개인 사정으로 전국 대회에 참여하지 못하는 학생들이 있어 참여가 가능한 학생 중 등수가 높은 3명을 선발하기로 했습니다.
+
+각 학생들의 선발 고사 등수를 담은 정수 배열 rank와 전국 대회 참여 가능 여부가 담긴 boolean 배열 attendance가 매개변수로 주어집니다.
+전국 대회에 선발된 학생 번호들을 등수가 높은 순서대로 각각 a, b, c번이라고 할 때 10000 × a + 100 × b + c를 return 하는 solution 함수를 작성해 주세요.
+"""
+
+# 내 풀이
+def solution(rank, attendance):
+    arr = []
+    stu_ran = {i:rank[i] for i in range(len(rank))}
+    stu_att = {i:attendance[i] for i in range(len(rank))}
+    
+    stu_ran = sorted(stu_ran.items(), key = lambda x: x[1])
+    
+    for s, r in stu_ran:
+        if stu_att[s]:
+            arr.append(s)
+            
+    return (10000*arr[0]) + (100*arr[1]) + arr[2]
+
+
+# 다른사람 풀이
+def solution(rank, attendance):
+    lst = []
+
+    for i in range(len(rank)):
+        if attendance[i]:
+            lst.append((rank[i],i))
+
+    lst.sort()
+
+    return 10000*lst[0][1]+100*lst[1][1]+lst[2][1]
