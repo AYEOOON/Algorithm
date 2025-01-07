c0cd686783a1650409071939de58f588e0159709
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2025-01-07 18:17:13 +0900

    Create 최소직사각형.java

---
+++ 프로그래머스/JAVA/1단계/최소직사각형.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int garo = sizes[0][0], sero = sizes[0][1];
+        
+        for(int[] s: sizes){
+            garo = Math.max(Math.max(s[0], s[1]), garo);
+            sero = Math.max(Math.min(s[0], s[1]), sero);
+        }
+        return garo * sero;
+    }
+}
