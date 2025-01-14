f22989db4aec3155ea7d046cbb4b6b1a61f4fbda
AYEOOON <101050134+AYEOOON@users.noreply.github.com>
2025-01-14 21:21:47 +0900

    solve[Heap]: 더 맵게

---
+++ 프로그래머스/JAVA/2단계/더 맵게.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> Q = new PriorityQueue<>();
+        for(int n: scoville){
+            Q.offer(n);
+        }
+        while(Q.peek() < K && Q.size() > 1){
+            Q.offer(Q.poll()+(Q.poll()*2));
+            answer++;
+        }
+        return Q.peek() >= K ? answer : -1;
+    }
+}
